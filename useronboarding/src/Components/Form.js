878ffe9bcf2ba3f7f0565c25e49e107dc65c3d2e
justinrivera47 <justinrivera47@gmail.com>
2021-12-15 19:50:16 -0500

    created the form fondation.

--- useronboarding/src/Components/Form.js
+++ useronboarding/src/Components/Form.js
@@ -3,7 +3,42 @@ import React from 'react'
 const Form = () => {
   return (
     <div>
-      
+      <h1>My Cool Form </h1>
+      <form>
+        <lable>Name:
+          <input 
+          type="text"
+          name="username"
+          value={username}
+          onChange={onChange}
+          />
+        </lable>
+        <lable>E-mail:
+          <input 
+          type="email"
+          name="email"
+          value={email}
+          onChange={onChange}
+          />
+        </lable>
+        <lable>Password:
+          <input 
+          type="password"
+          name="password"
+          value={password}
+          onChange={onChange}
+          />
+        </lable>
+        <lable>Terms of Service:
+          <input 
+          type="checkbox"
+          name="tos"
+          checked={checked}
+          onChange={onChange}
+          />
+        </lable>
+          <input type='submit' value='Create a Friend!' />
+      </form>
     </div>
   )
 }
